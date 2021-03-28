d0b203be414d8165690b52e684b287249aede205
Marin0FF <hmarinov1977@gmail.com>
2021-03-28 21:24:35 +0100

    finalized color variables

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const Donation = mongoose.model('Donations');
+const Donation = mongoose.model('Donation');
 const Account = mongoose.model('Account');
 
 exports.showDashboard = (req, res) => {
