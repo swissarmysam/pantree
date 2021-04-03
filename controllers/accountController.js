32889666c8a6b6091473f4916eefc63f82ebea11
swissarmysam <s1804867@connect.glos.ac.uk>
2021-04-03 19:17:08 +0100

    reupload controllers

---
+++ controllers/accountController.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const Account = mongoose.model('Account');
+const promisify = require('es6-promisify');
+
+exports.registerForm = (req, res) => {
+  res.render('register', { title: 'Register' });
+};
+
+exports.validateRegister = (req, res, next) => {
+  req.sanitizeBody('name');
+  req.checkBody('name', 'Please enter your name!').notEmpty();
+  req.checkBody('email', 'Please use a valid email!').isEmail();
+  req.sanitizeBody('email').normalizeEmail({
+    gmail_remove_dots: false,
+    remove_extension: false,
+    gmail_remove_subaddress: false,
+  });
+  req.checkBody('password', 'A password is required!').notEmpty();
+  req
+    .checkBody('password-confirm', 'Confirmed password cannot be blank!')
+    .notEmpty();
+  req
+    .checkBody('password-confirm', 'Oops! Your passwords do not match!')
+    .equals(req.body.password);
+
+  const errors = req.validationErrors();
+  if (errors) {
+    req.flash('error', errors.map(err => err.msg));
+    res.render('register', {
+      title: 'Register',
+      body: req.body,
+      flashes: req.flash(),
+    });
+    return; // stop the fn from running
+  }
+  next(); // there were no errors!
+};
+
+exports.register = async (req, res, next) => {
+  const account = new Account({
+    email: req.body.email,
+    name: req.body.name,
+  });
+  const register = promisify(Account.register, Account);
+  await register(account, req.body.password);
+  next(); // pass to authController.login
+};
+
+exports.account = (req, res) => {
+  res.render('account', { title: 'Edit Account' });
+};
+
+exports.updateAccount = async (req, res) => {
+  const updates = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const account = await Account.findOneAndUpdate(
+    { _id: req.account._id },
+    { $set: updates },
+    { new: true, runValidators: true, context: 'query' }
+  );
+  req.flash('success', 'Updated the profile!');
+  res.redirect('back');
+};
---
+++ controllers/dashboardController.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const Donation = mongoose.model('Donation');
+const Account = mongoose.model('Account');
+
+exports.showDonations = (req, res) => {
+  res.render('donations', { title: 'Donations' });
+};
