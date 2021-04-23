a00e57b382a39235819c925ccd964ffe9dd4777f
Marin0FF <hmarinov1977@gmail.com>
2021-04-23 09:32:05 +0100

    Updated setup view & form handler

--- public/dist/js/formHandler.js
+++ public/dist/js/formHandler.js
@@ -1,28 +1,27 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-use-before-define */
-// if the form has multiple steps run the block below
-if (document.body.contains(document.querySelector('.steps'))) {
-  const formSections = document.querySelectorAll('[section]');
-  const stepCont = document.querySelector('.steps');
-  renderSteps(formSections, stepCont);
-  renderSection(formSections, 0);
+export function initForm(handlersArr) {
+  if (document.body.contains(document.querySelector('.steps'))) {
+    const formSections = document.querySelectorAll('[section]');
+    const stepContainer = document.querySelector('.steps');
+    renderSteps(formSections, stepContainer);
+    renderSection(formSections, 0, handlersArr);
+  }
 }
-// else render a submit button
-// renders the the active section
-function renderSection(formSections, switchToSection) {
+function renderSection(formSections, switchToSection, handlersArr) {
   const section = formSections[switchToSection];
   const button = document.querySelector('.button.primary-action');
-  // fn veriable stores a function that handles the form section
-  // function must match the value of the section attribute
-  const fn = window[section.getAttribute('section')];
+  const backBtn = document.querySelector('.secondary-action');
+  const fn = handlersArr[switchToSection];
   renderTitle(section);
-  if (typeof fn === 'function') fn.apply(null, [section, formSections, button]);
+  if (typeof fn === 'function')
+    fn.apply(null, [section, formSections, button, backBtn]);
   formSections.forEach((item, i) => {
-    item.style.display = switchToSection === i ? 'block' : 'none';
+    item.style.display = switchToSection === i ? null : 'none';
   });
 }
 // creates the step component
-function renderSteps(formSections, stepCont) {
+function renderSteps(formSections, stepContainer) {
   for (let i = 0; i < formSections.length; i++) {
     const step = document.createElement('li');
     const stepMarker = document.createElement('span');
@@ -30,15 +29,15 @@ function renderSteps(formSections, stepCont) {
     step.classList.add('steps-segment');
     stepMarker.classList.add('steps-marker');
     if (i === formSections.length - 1) {
-      const iconCont = document.createElement('span');
+      const iconContainer = document.createElement('span');
       const icon = document.createElement('i');
-      iconCont.classList.add('icon');
+      iconContainer.classList.add('icon');
       icon.classList.add('fa', 'fa-check');
-      iconCont.appendChild(icon);
-      stepMarker.appendChild(iconCont);
+      iconContainer.appendChild(icon);
+      stepMarker.appendChild(iconContainer);
     }
     step.append(stepMarker);
-    stepCont.appendChild(step);
+    stepContainer.appendChild(step);
   }
 }
 // renders the section title
@@ -46,70 +45,50 @@ function renderTitle(section) {
   const title = document.querySelector('.form-title');
   title.innerHTML = section.getAttribute('section-name');
 }
-// helper function, returns the section currently displayed
+// returns the section currently displayed
 function currentSection(formSections) {
   let sectionIndex;
   formSections.forEach((section, i) => {
     if (section.style.display !== 'none') {
       sectionIndex = i;
+      console.log(sectionIndex);
     }
   });
   return sectionIndex;
 }
 // renders the primary button
-function renderButton(type, label, button) {
+export function renderButton(type, label, button, clickable = false) {
   button.type = type;
   button.value = label;
   button.textContent = label;
+  button.disabled = clickable;
 }
-
-function nextSection(formSections) {
-  const section = currentSection(formSections) + 1;
-  renderSection(formSections, section);
+// renders the secondary button
+export function renderBackButton(backBtn, visible) {
+  backBtn.style.display = visible === true ? 'block' : 'none';
+}
+// navigates to the section called from a form-section function
+// selectedSection will eiher add or substact 1 from the current section index
+export function switchSection(
+  formSections,
+  handlersArr,
+  switchDirection = 'prev'
+) {
+  const selectedSection = switchDirection === 'next' ? 1 : -1;
+  console.log('currentSection', currentSection(formSections));
+  const section = currentSection(formSections) + selectedSection;
+  renderSection(formSections, section, handlersArr);
   switchStep(section);
+  console.log('selectedSection', selectedSection);
+  console.log('section', section);
 }
 // updates the step component
 function switchStep(sectionIndex) {
   const steps = document.querySelectorAll('.steps-segment');
   steps.forEach((item, i) => {
-    console.log(i);
     if (sectionIndex === i) {
       item.classList.add('is-active');
       steps[i - 1].classList.remove('is-active');
     }
-  });
-}
-// The rest of these functions below handle the individual sections
-// They need to match the 'section' data attribute in order to get called
-function userType(section, formSections, button) {
-  renderButton('button', 'Next', button);
-  button.addEventListener('click', () => {
-    // validate section inputs
-    console.log(section);
-    button.replaceWith(button.cloneNode(true));
-    // if validation is successful switch to next section
-    nextSection(formSections);
-  });
-}
-
-function accountInfo(section, formSections, button) {
-  renderButton('submit', 'Submit', button);
-  button.addEventListener('click', () => {
-    // validate section inputs
-    console.log(section);
-    button.replaceWith(button.cloneNode(true));
-    // if validation is successful switch to next section
-    nextSection(formSections);
-  });
-}
-
-function accountVerification(section, formSections, button) {
-  renderButton('button', 'Resend', button);
-  button.addEventListener('click', () => {
-    // validate section inputs
-    console.log(section);
-    button.replaceWith(button.cloneNode(true));
-    // if validation is successful switch to next section
-    nextSection(formSections);
   });
 }
